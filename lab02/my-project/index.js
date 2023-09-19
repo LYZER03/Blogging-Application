fff7788e27fb314a6284ab75834de2ca9d4cf067
Alex Dong <dongalex93@gmail.com>
2023-09-19 14:52:51 +0200

    "Organize the source code in a module"

--- lab02/my-project/index.js
+++ lab02/my-project/index.js
@@ -2,27 +2,9 @@ console.log("Hello Node.js!")
 
 // Import a module
 const http = require('http')
-// Import Node url module
-const url = require('url')
-const qs = require('querystring')
-
-const serverHandle = function (req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
-
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
-  
-  res.end()
-}
+const handles = require('./handles')
 
 // Declare an http server
 http
-.createServer(serverHandle)
+.createServer(handles.serverHandle)
 .listen(8080)

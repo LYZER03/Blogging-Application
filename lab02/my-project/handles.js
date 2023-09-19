e010f1fded6fb2902bd0f8517da6d29de38a5909
Alex Dong <dongalex93@gmail.com>
2023-09-19 16:10:04 +0200

    build: (5) Create a basic application

--- lab02/my-project/handles.js
+++ lab02/my-project/handles.js
@@ -1,23 +1,50 @@
+const url = require('url');
+const qs = require('querystring');
 
-// Necessary imports
-// Import Node url module
-const url = require('url')
-const qs = require('querystring')
+const content = '<!DOCTYPE html>' +
+'<html><body>'+
+    '<h1>Welcome to the Hello App!</h1>'+
+    '<p>Usage:</p>'+
+    '<ul>'+
+        '<li><a href="/hello?name=John">/hello?name=John</a> - Say hello to John</li>'+
+        '<li><a href="/hello?name=Alex">/hello?name=Alex</a> - Introduce Yourself</li>'+
+    '</ul>'+
+'</body></html>'
+
+
+const serverHandle = (req, res) =>{
+    const route = url.parse(req.url);
+    const params = qs.parse(route.query);
+
+    if (route.pathname === '/') {
+        // Route: /
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(content);
+        res.end();
+
+    } else if (route.pathname === '/hello') {
+        // Route: /hello
+        const name = params.name;
+        if (!name) {
+            // If the 'name' query parameter is missing
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Bad Request: Missing name query parameter');
+        } else if (name === 'Alex') {
+            // If 'name' is 'YourName', reply with a short intro
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Hello, I am Alex, a student of ECE.');
+        } else {
+            // For any other 'name', reply with a hello message
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(`Hello, ${name}!`);
+        }
+    } else {
+        // For any other route, reply with a 404 Not Found response
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+};
 
 module.exports = {
-    serverHandle : function (req, res) {
-        const route = url.parse(req.url)
-        const path = route.pathname 
-        const params = qs.parse(route.query)
-
-        res.writeHead(200, {'Content-Type': 'text/plain'})
-
-        if (path === '/hello' && 'name' in params) {
-            res.write('Hello ' + params['name'])
-        } else {
-            res.write('Hello anonymous')
-        }
-
-        res.end()
-    }
-  }
+    serverHandle,
+};

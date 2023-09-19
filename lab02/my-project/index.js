4f2dee1141eed355d7260162a63095bb5b900b65
Alex Dong <dongalex93@gmail.com>
2023-09-19 14:42:37 +0200

    feat: (3.7) add basic routing

--- lab02/my-project/index.js
+++ lab02/my-project/index.js
@@ -6,29 +6,19 @@ const http = require('http')
 const url = require('url')
 const qs = require('querystring')
 
-// Define a string constant concatenating strings
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World!</p>' +
-'    </body>' +
-'</html>'
-
 const serverHandle = function (req, res) {
-  // Retrieve and print the queryParams
-  const queryParams = qs.parse(url.parse(req.url).query)
-  console.log(queryParams)
+  const route = url.parse(req.url)
+  const path = route.pathname 
+  const params = qs.parse(route.query)
 
-  // Write a response header
-  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.writeHead(200, {'Content-Type': 'text/plain'})
 
-  //Write a response content
-  res.write(content)
-
+  if (path === '/hello' && 'name' in params) {
+    res.write('Hello ' + params['name'])
+  } else {
+    res.write('Hello anonymous')
+  }
+  
   res.end()
 }
 

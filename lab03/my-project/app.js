74c0aab07bce419f5d03afe65ac7715b45eb22b1
dylked <dylked2011@gmail.com>
2023-09-23 17:34:14 +0200

    build: (2.3) Create a set of API routes

--- lab03/my-project/app.js
+++ lab03/my-project/app.js
@@ -65,10 +65,12 @@ const db = {
     ]
 }
 
+// list all articles
 app.get('/articles', (req, res) => {
     res.status(200).json(db.articles);
 });
 
+// add a new article
 app.post('/articles', (req, res) => {
     const { title, content, date, author } = req.body;
 
@@ -88,6 +90,7 @@ app.post('/articles', (req, res) => {
     res.status(201).json(newArticle);
 });
 
+// get an article by ID
 app.get('/articles/:articleId', (req, res) => {
     const article = db.articles.find(a => a.id === req.params.articleId);
     if (article) {
@@ -95,6 +98,50 @@ app.get('/articles/:articleId', (req, res) => {
     } else {
         res.status(404).send('Article not found');
     }
+});
+
+// get all comments of the article with articleId
+app.get('/articles/:articleId/comments', (req, res) => {
+    const articleId = req.params.articleId;
+    const commentsForArticle = db.comments.filter(comment => comment.articleId === articleId);
+    res.json(commentsForArticle);
+});
+
+// add a new comment to a specific article with articleId
+app.post('/articles/:articleId/comments', (req, res) => {
+    const articleId = req.params.articleId;
+    const { content, author } = req.body;
+
+
+    const articleExists = db.articles.some(article => article.id === articleId);
+    if (!articleExists) {
+        return res.status(404).json({ error: 'Article not found' });
+    }
+
+    const newComment = {
+        id: uuidv4(),
+        timestamp: Date.now(),
+        content,
+        articleId,
+        author
+    };
+
+    db.comments.push(newComment);
+    res.status(201).json(newComment);
+});
+
+// get a comment with commentId of the article with articleId
+app.get('/articles/:articleId/comments/:commentId', (req, res) => {
+    const articleId = req.params.articleId;
+    const commentId = req.params.commentId;
+
+    const targetComment = db.comments.find(comment => comment.id === commentId && comment.articleId === articleId);
+
+    if (!targetComment) {
+        return res.status(404).json({ error: 'Comment not found for the given article' });
+    }
+
+    res.json(targetComment);
 });
 
 app.use((req, res) => {

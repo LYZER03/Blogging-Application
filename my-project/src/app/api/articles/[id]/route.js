ef7e343594bb6174c4652240027e930df549f898
Alex Dong <dongalex93@gmail.com>
2023-10-25 12:05:37 +0200

    ci: add tailwind css

--- my-project/src/app/api/articles/[id]/route.js
+++ my-project/src/app/api/articles/[id]/route.js
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
-import { mockData } from "./mockdata";
+import { mockData } from "../mockdata";
 
 export const GET = async (request) => {
   const { id } = request.params; // Get the article ID from the URL
-
+  console.log(params)
   // Find the article with the matching ID
   const article = mockData.find((item) => item.id === Number(id));
 
--- my-project/src/app/articles/[id]/page.jsx
+++ my-project/src/app/articles/[id]/page.jsx
@@ -1,59 +1,56 @@
-import React from "react";
+"use client"
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { notFound } from "next/navigation";
 
-async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/articles/${id}`, {
-    cache: "no-store",
-  });
+async function fetchData(id) {
+  try {
+    const apiUrl = `http://localhost:3000/api/articles/${id}`;
+    const res = await fetch(apiUrl, { cache: "no-store" });
 
-  if (!res.ok) {
-    return notFound()
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    console.log(res.json())
+    return res.json();
+  } catch (error) {
+    throw error;
+  }
+}
+
+const Article = ({ data }) => {
+  if (!data) {
+    return <div>Loading...</div>;
   }
 
-  return res.json();
-}
-
-
-export async function generateMetadata({ params }) {
-
-  const post = await getData(params.id)
-  return {
-    title: post.title,
-    description: post.desc,
-  };
-}
-
-const article = async ({ params }) => {
-  const data = await getData(params.id);
   return (
     <div>
       <div className="flex">
         <div className="flex-1 flex flex-col justify-between">
           <h1 className="text-2xl">{data.title}</h1>
-          <p className="text-base font-light">
-            {data.desc}
-          </p>
-          <div className="flex items-center space-x-4">
-            <span className={styles.username}>{data.username}</span>
-          </div>
+          <p className="text-base font-light">{data.desc}</p>
         </div>
         <div className="flex-1 h-72 relative">
-          <Image
-            src={data.img}
-            alt=""
-            fill={true}
-            objectFit="cover"
-          />
+          <Image src={data.img} alt={data.altText} fill={true} objectFit="cover" />
         </div>
       </div>
       <div className="mt-12 text-lg font-light text-gray-600 text-justify">
-        <p>
-         {data.content}
-        </p>
+        <p>{data.content}</p>
       </div>
     </div>
   );
 };
 
-export default article;
+const ArticlePage = ({ params }) => {
+  const [articleData, setArticleData] = useState(null);
+
+  useEffect(() => {
+    fetchData(params.id)
+      .then((data) => setArticleData(data))
+      .catch((error) => console.error("Error while fetching data:", error));
+  }, [params.id]);
+
+  return <Article data={articleData} />;
+};
+
+export default ArticlePage;

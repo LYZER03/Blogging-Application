d5aa66e5bf169516dda045bfd6a6224f741ce3a6
Dylan Nuñez <dylan@logoscorp.com>
2023-11-14 11:13:16 +0100

    style: moving files from my project folder to root

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
-import Footer from '@/components/footer/footer';
-import Navbar from '@/components/navbar/navbar';
+import Footer from '../components/footer/Footer';
+import Navbar from '../components/navbar/Navbar';
 
 export const metadata = {
   title: 'Create Next App',

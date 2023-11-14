721c512efd181770948d6b1fdafbcba8fce82ddd
Dylan Nuñez <dylan@logoscorp.com>
2023-11-14 13:07:44 +0100

    feat: Include: user profile API and user icon in nav menu

---
+++ src/app/api/profile/mockdata.js
@@ -0,0 +1,14 @@
+export const mockData = [
+    {
+      userName: "Dylan",
+      email:"dylan@edu.ece.fr"
+    },
+    {
+      userName: "Narcys",
+      email:"dylan@edu.ece.fr"
+    },
+    {
+      userName: "aleNarcysx",
+      email:"narcys@edu.ece.fr"
+    }
+]
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,29 @@ import Link from 'next/link'
 import React from 'react'
 
 
+async  function getData() {
+  const apiUrl = "http://localhost:3000/api/profile";
+  console.log("Fetching data from:", apiUrl);
+
+  try {
+    const res = await fetch(apiUrl, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      console.error("Error response:", res.status, res.statusText);
+      throw new Error("Failed to fetch data");
+    }
+
+    const dataProfile = await res.json();
+    console.log("Data fetched successfully:", dataProfile);
+    return dataProfile;
+  } catch (error) {
+    console.error("Error while fetching data:", error);
+    throw error;
+  }
+}
+
 const links = [
   {
     id:1,
@@ -22,10 +45,11 @@ const links = [
     id:4,
     title:"contact",
     url:"/contact"
-  },
+  }
 ];
 
-const Navbar = () => {
+const Navbar = async () => {
+  const dataProfile = await getData();
   return (
     <div className="h-16 flex justify-between items-center">
       <Link className="font-bold text-2xl" href='/'>
@@ -37,6 +61,12 @@ const Navbar = () => {
             {link.title}
           </Link>
         ))}
+      </div>
+      <div>
+          <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+            <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+          </div>
+          <span className="float-right">{dataProfile.userName}</span>
       </div>
     </div>
   );

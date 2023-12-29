f8954e0855adf503308297ce9ef6d6c4e2e0bb01
DONGAlex03 <lyzerdong03@gmail.com>
2023-12-29 23:36:54 +0100

    feat: add edit functionnality

--- app/src/app/articles/[id]/page.jsx
+++ app/src/app/articles/[id]/page.jsx
@@ -13,6 +13,31 @@ export default function ArticlePage({ params }) {
   const [article, setArticle] = useState(null)
   const [loading, setLoading] = useState(true)
   const [isAuthor, setIsAuthor] = useState(false);
+
+  const [editing, setEditing] = useState(false);
+  const [editedArticle, setEditedArticle] = useState({
+    title: articles.title,
+    content: articles.content,
+    newImage: null, // Champ d'entrée pour la nouvelle image
+    category: articles.category,
+  });
+
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleEditCancel = () => {
+    setEditing(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value, files } = e.target;
+    
+    // Si le champ d'entrée est de type "file", cela signifie qu'il s'agit du champ de la nouvelle image
+    const newValue = name === 'newImage' ? files[0] : value;
+
+    setEditedArticle((prev) => ({ ...prev, [name]: newValue }));
+  };
   
   useEffect(() => {
     const checkAuthor = async () => {
@@ -43,6 +68,46 @@ export default function ArticlePage({ params }) {
       }
     }
   };
+
+  const handleEditSubmit = async () => {
+    // Si une nouvelle image est fournie, téléchargez-la
+    let newImageUrl = editedArticle.image_url;
+    if (editedArticle.newImage) {
+      const fileExt = editedArticle.newImage.name.split('.').pop();
+      const fileName = `${Math.random()}.${fileExt}`;
+      const { data: image, error: uploadError } = await supabase.storage
+        .from('article-images') 
+        .upload(fileName, editedArticle.newImage);
+
+      if (uploadError) {
+        console.error('Error uploading new image', uploadError);
+        return;
+      }
+
+      const { data } = await supabase.storage.from('article-images').getPublicUrl(fileName);
+      newImageUrl = data.publicUrl;
+    }
+
+    // Effectuez la mise à jour de l'article avec les données modifiées
+    const { error } = await supabase
+      .from('articles')
+      .update({
+        title: editedArticle.title,
+        content: editedArticle.content,
+        image_url: newImageUrl,
+        category: editedArticle.category,
+      })
+      .eq('id', id);
+
+    if (error) {
+      console.error('Error updating the article', error);
+    } else {
+      setEditing(false);
+      // Rechargez l'article après la mise à jour
+      fetchArticle(id);
+    }
+  };
+
 
   const fetchArticle = async (articleId) => {
     if (!articleId) return
@@ -108,12 +173,90 @@ export default function ArticlePage({ params }) {
       <div className="text-lg text-gray-700 leading-relaxed">
         <p>{article.content}</p>
       </div>
-      {isAuthor && (
-        <button onClick={handleDelete} className="bg-red-500 text-white px-4 py-2 rounded mt-4">
-          Delete Article
-        </button>
-      )}
 
+      {isAuthor && (
+        <>
+          <button
+            onClick={handleDelete}
+            className="bg-red-500 text-white px-4 py-2 rounded mt-4"
+            style={{marginRight: '8px'}}
+          >
+            Delete Article
+          </button>
+
+          <button
+            onClick={handleEditClick}
+            className="bg-blue-500 text-white px-4 py-2 rounded mt-4 mr-2"
+          >
+            Edit Article
+          </button>
+
+          {editing && (
+            <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex items-center justify-center">
+              <div className="bg-white p-6 rounded-lg">
+                <h2 className="text-2xl font-bold mb-4">Edit Article</h2>
+                <label htmlFor="editTitle" className="block mb-2">
+                  Title:
+                  <input
+                    type="text"
+                    id="editTitle"
+                    name="title"
+                    value={editedArticle.title}
+                    onChange={handleInputChange}
+                    className="w-full border p-2"
+                  />
+                </label>
+                <label htmlFor="editContent" className="block mb-2">
+                  Content:
+                  <input
+                    type="text"
+                    id="editContent"
+                    name="content"
+                    value={editedArticle.content}
+                    onChange={handleInputChange}
+                    className="w-full border p-2"
+                  />
+                </label>
+                <label htmlFor="editCategory" className="block mb-2">
+                  Category:
+                  <input
+                    type="text"
+                    id="editCategory"
+                    name="category"
+                    value={editedArticle.category}
+                    onChange={handleInputChange}
+                    className="w-full border p-2"
+                  />
+                </label>
+                <label htmlFor="editNewImage" className="block mb-2">
+                  New Image:
+                  <input
+                    type="file"
+                    id="editNewImage"
+                    name="newImage"
+                    accept="image/*"
+                    onChange={handleInputChange}
+                    className="w-full border p-2"
+                  />
+                </label>
+
+                <button
+                  onClick={handleEditSubmit}
+                  className="bg-blue-500 text-white px-4 py-2 rounded mt-4 mr-2"
+                >
+                  Save
+                </button>
+                <button
+                  onClick={handleEditCancel}
+                  className="bg-gray-500 text-white px-4 py-2 rounded mt-4"
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          )}
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
